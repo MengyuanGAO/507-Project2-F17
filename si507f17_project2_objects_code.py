85b25dfc1bebe884572e183ad90098aef68c2ae8
Mengyuan Gao <sophia.world1992@gmail.com>
2018-04-27 15:16:58 -0700

    Update si507f17_project2_objects_code.py

--- si507f17_project2_objects_code.py
+++ si507f17_project2_objects_code.py
@@ -1,39 +1,11 @@
-# coding=utf-8
-## SI 507 F17 Project 2 - Objects
 import requests
 import json
 import unittest
 import csv
 
-## Instructions for each piece to be completed for this project can be found in the file, below.
 
-## To see whether your problem solutions are passing the tests, you should run the Python file:
-# si507f17_project2_objects_tests.py, which should be saved in the same directory as this file. 
-
-## (DO NOT change the name of this file! Make sure to re-save it with the name si507f17_project2_objects_code.py if you change the name. Otherwise, we will not be able to grade it!)
-
-
-print("\n*** *** PROJECT 2 *** ***\n")
-
-
-## Useful additional references for this part of the homework from outside class material:
-## - the iTunes Search API documentation:
-## - the following chapters from the textbook (also referred to in SI 506): https://www.programsinformationpeople.org/runestone/static/publicpy3/Classes/ThinkingAboutClasses.html, https://www.programsinformationpeople.org/runestone/static/publicpy3/Classes/ClassesHoldingData.html, https://www.programsinformationpeople.org/runestone/static/publicpy3/UsingRESTAPIs/cachingResponses.html
-## - and possibly other chapters, as a reference!
-
-## The first problem to complete for this project can be found below.
-
-
-#########
-
-
+######### Part One
 ## You can search for a variety of different types of media with the iTunes Search API: songs, movies, ebooks and audiobooks... (and more) You'll definitely need to check out the documentation to understand/recall how the parameters of this API work: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
-
-## Here, we've provided functions to get and cache data from the iTunes Search API, but looking at the information in that documentation will help you understand what is happening when the second function below gets invoked. 
-## Make sure you understand what the function does, how it works, and how you could invoke it to get data from iTunes Search about e.g. just songs corresponding to a certain search term, just movies, or just books. 
-## Refer to the textbook sections about caching, linked above, to help understand these functions!
-
-## You may want to try them out and see what data gets returned, in order to complete the problems in this project.
 
 def params_unique_combination(baseurl, params_d, private_keys=['api_key']):
     alphabetized_keys = sorted(params_d.keys())
@@ -68,12 +40,10 @@ def sample_get_cache_itunes_data(search_term, media_term="all"):
         cache_file_ref.close()
         return CACHE_DICTION[unique_ident]
 
-
-## [PROBLEM 1] [250 POINTS]
-print("\n***** PROBLEM 1 *****\n")
-
-## For problem 1, you should define a class Media, representing ANY piece of media you can find on iTunes search.
-
+    
+    
+###### Part Two
+## define a class Media, representing ANY piece of media you can find on iTunes search.
 
 ## The Media class should accept one dictionary data structure representing a piece of media from iTunes as input to the constructor.
 ## Its constructor should invoke a method to get and cache data, and instatiate at least the following instance variables:
@@ -109,14 +79,10 @@ class Media(object):
     def __contains__(self, term):
         return term in self.title
 
-
-## [PROBLEM 2] [400 POINTS]
-print("\n***** PROBLEM 2 *****\n")
-
-
-## In 2 parts.
-
-## Now, you'll define 2 more different classes, each of which *inherit from* class Media:
+    
+    
+##### Part Three
+#define 2 more different classes, each of which *inherit from* class Media:
 ## class Song
 ## class Movie
 
@@ -136,7 +102,7 @@ print("\n***** PROBLEM 2 *****\n")
 
 class Song(Media):
     def __init__(self, media_info_dict):
-        super().__init__(media_info_dict) 
+        super().__init__(media_info_dict) #
         self.type = 'songs'
         self.album = media_info_dict['collectionName']
         self.track_number = media_info_dict['trackNumber']
@@ -185,10 +151,9 @@ class Movie(Media):
             return len(self.description.split())
 
 
-## [PROBLEM 3] [150 POINTS]
-print("\n***** PROBLEM 3 *****\n")
-
-## In this problem, you'll write some code to use the definitions you've just written.
+        
+#### Part Five
+## write some code to use the definitions you've just written.
 
 ## First, here we have provided some variables which hold data about media overall, songs, movies, and books.
 
@@ -214,9 +179,7 @@ song_list = [Song(media_info_dict) for media_info_dict in song_samples]
 movie_list = [Movie(media_info_dict) for media_info_dict in movie_samples]
 
 
-## [PROBLEM 4] [200 POINTS]
-print("\n***** PROBLEM 4 *****\n")
-
+###### Part Five
 ## Finally, write 3 CSV files:
 # - movies.csv
 # - songs.csv
@@ -229,19 +192,6 @@ print("\n***** PROBLEM 4 *****\n")
 # - url (for the itunes url of that thing -- the url to view that track of media on iTunes) 
 # - length 
 
-## There are no provided tests for this problem -- you should check your CSV files to see that they fit this description to see if this problem worked correctly for you. IT IS VERY IMPORTANT THAT YOUR CSV FILES HAVE EXACTLY THOSE NAMES!
-
-## You should use the variables you defined in problem 3, iteration, and thought-out use of accessing elements of a class instance, to complete this!
-
-## HINT: You may want to think about what code could be generalized here, and what couldn't, and write a function or two -- that might make your programming life a little bit easier in the end, even though it will require more thinking at the beginning! But you do not have to do this.
-
-## HINT #2: *** You MAY add other, non-required, methods to the class definitions in order to make this easier, if you prefer to! 
-
-## It is perfectly fine to write this code in any way, as long as you rely on instances of the classes you've defined, and the code you write results in 3 correctly formatted CSV files!
-
-## HINT #3: Check out the sections in the textbook on opening and writing files, and the section(s) on CSV files!
-
-## HINT #4: Write or draw out your plan for this before you actually start writing the code! That will make it much easier.
 
 
 FIELD_NAMES = ['title', 'artist', 'id', 'url', 'length']
